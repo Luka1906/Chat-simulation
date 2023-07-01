73edc25f5802c60085b4d49fa207d1da5d9f366d
Luka Matovic <lukamatovic1994@gmail.com>
2023-07-01 10:09:26 -0500

    Changing port

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ io.on(`connection`,(socket) => {
         delete users[socket.id];
     })
 });
-server.listen(3000);
+const port = process.env.PORT || 3000
+server.listen(port, ()=> console.log(`Server is running on ${port}`));
 
 
